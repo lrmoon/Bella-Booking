6719b5c3890959d8688b91ce0721293db54a4ece
lrmoon <larariad@rocketmail.com>
2023-05-30 16:35:22 -0400

    template config

--- bella_booking/settings.py
+++ bella_booking/settings.py
@@ -15,6 +15,7 @@ import os
 import environ
 
 env = environ.Env()
+
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,7 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+#STATIC_ROOT = 'static'
+
 STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
