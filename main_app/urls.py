aa3e48f63b20272a231798d19cb84de5f8cc5957
lrmoon <larariad@rocketmail.com>
2023-06-16 14:30:09 -0400

    integration

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,18 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
     path("services/", views.services, name="services"),
+    path("portfolio/", views.portfolio, name="portfolio"),
+    path("blog/", views.blog, name="blog"),
+    path("contact/", views.contact, name="contact"),
+
+    path("team/", views.contact, name="team"),
+    path("pricing/", views.contact, name="pricing"),
+    path("eye-makeup/", views.contact, name="eye-makeup"),
+
+    path("404/", views.nope404, name="404"),
+    path("coming-soon/", views.comingSoon, name="coming-soon")
+    
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,14 +7,37 @@ def index(request):
 def services(request):
     return render(request, 'main_app/services.html')
 
-def index(request):
-    return render(request, 'index.html')
+def about(request):
+    return render(request, 'main_app/about.html')
 
-def index(request):
-    return render(request, 'index.html')
+def portfolio(request):
+    return render(request, 'main_app/portfolio.html')
 
-def index(request):
-    return render(request, 'index.html')
+def blog(request):
+    return render(request, 'main_app/blog.html')
+
+def contact(request):
+    return render(request, 'main_app/contact.html')
+
+
+#subpages render
+def team(request):
+    return render(request, 'main_app/pages/team.html')
+
+def pricing(request):
+    return render(request, 'main_app/pages/pricing.html')
+
+def eyeMakeup(request):
+    return render(request, 'main_app/pages/service-type/eye-makeup.html')
+
+
+
+def nope404(request):
+    return render(request, 'main_app/pages/misc/404.html')
+
+def comingSoon(request):
+    return render(request, 'main_app/pages/misc/coming-soon.html')
+
 
 
 
