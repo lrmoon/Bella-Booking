2dea88c636639aa1928fa1e4a5a589e5c6c25349
lrmoon <larariad@rocketmail.com>
2023-07-04 20:25:54 -0400

    finalizingg

--- main_app/urls.py
+++ main_app/urls.py
@@ -14,14 +14,14 @@ urlpatterns = [
 
     path("pages/team/", views.team, name="team"),
     path("pages/pricing/", views.pricing, name="pricing"),
-    path("pages/pages-services/", views.eyeMakeup, name="eye-makeup"),
+    path("pages/pages-services/eye-makeup", views.eyeMakeup, name="eye-makeup"),
     path("pages/pages-services/hair-makeup/", views.hairMakeup, name="hair-makeup"),
     path("pages/pages-services/face-makeup/", views.faceMakeup, name="face-makeup"),
     path("pages/pages-services/bridal-makeup/", views.bridalMakeup, name="bridal-makeup"),
     path("pages/pages-services/fashion-makeup/", views.fashionMakeup, name="fashion-makeup"),
-    path("fantastic-makeup/", views.fantasticMakeup, name="fantastic-makeup"),
-    path("effect-makeup/", views.effectMakeup, name="effect-makeup"),
-    path("child-face-painting/", views.childFacePainting, name="child-face-painting"),
+    path("pages/pages-services/fantastic-makeup/", views.fantasticMakeup, name="fantastic-makeup"),
+    path("pages/pages-services/effect-makeup/", views.effectMakeup, name="effect-makeup"),
+    path("pages/pages-services/child-face-painting/", views.childFacePainting, name="child-face-painting"),
     
     
     path("404/", views.nope404, name="404"),
