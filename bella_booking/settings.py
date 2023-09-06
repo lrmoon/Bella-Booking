48b1bfcf7b5862b016458b514ba8e3d965727cfa
lrmoon <larariad@rocketmail.com>
2023-09-06 14:38:37 -0400

    whitenoise config

--- bella_booking/settings.py
+++ bella_booking/settings.py
@@ -135,11 +135,11 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL ='/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 #STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
