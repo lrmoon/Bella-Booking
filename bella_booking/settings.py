a356a6bbe15fca501e29c89df507230f4cf9b7a4
lrmoon <larariad@rocketmail.com>
2023-09-06 14:00:01 -0400

    hosts

--- bella_booking/settings.py
+++ bella_booking/settings.py
@@ -20,7 +20,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ["bella-beauty.onrender.com", "localhost"]
+ALLOWED_HOSTS = ['bella-beauty.onrender.com', 'localhost']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
